fe71f87302692e17c098d4b0d5e33783a9d76a8d
LiaOS Framework <medantikettt@gmail.com>
2025-08-06 21:01:36 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,18 +1,23 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+USERNAME = os.getenv("LIAOS_USER")
+PASSWORD = os.getenv("LIAOS_PASS")
 
 def login():
     st.title("Login LiaOS – Polda")
+
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if username == "admin" and password == "1234":
-            st.session_state["role"] = "admin"
+        if username == USERNAME and password == PASSWORD:
             st.session_state["logged_in"] = True
-            st.experimental_rerun()
-        elif username == "humas" and password == "1234":
-            st.session_state["role"] = "user"
-            st.session_state["logged_in"] = True
-            st.experimental_rerun()
+            st.session_state["role"] = "Admin"
+            st.success("✅ Login berhasil! Silakan pilih menu di sidebar.")
         else:
-            st.error("Username atau password salah")
+            st.error("❌ Username atau password salah.")
