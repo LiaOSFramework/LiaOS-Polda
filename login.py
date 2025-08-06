cd41e4cf96576cfc5a04b8bba6b0e336d3ccff38
LiaOS Framework <medantikettt@gmail.com>
2025-08-06 21:07:07 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,15 @@
-if username == "polda" and password == "liaos123":
+import streamlit as st
+
+def login():
+    st.header("Login LiaOS – Polda")
+    
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    
+    if st.button("Login"):
+        if username == "polda" and password == "liaos123":
+            st.session_state["logged_in"] = True
+            st.session_state["role"] = "admin"
+            st.success("Login berhasil")
+        else:
+            st.error("❌ Username atau password salah.")
